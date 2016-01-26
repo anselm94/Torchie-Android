1058c8b64a26b254be06a16b5637ba4898ff4ec5
Merbin J Anselm <merbinjanselm@gmail.com>
2016-01-26 08:41:21 +0530

    Added Preference listeners

--- app/src/main/java/in/blogspot/anselmbros/torchie/ui/fragment/SettingsFragment.java
+++ app/src/main/java/in/blogspot/anselmbros/torchie/ui/fragment/SettingsFragment.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.SwitchCompat;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +22,7 @@ import android.widget.TextView;
 import in.blogspot.anselmbros.torchie.R;
 import in.blogspot.anselmbros.torchie.misc.TorchieConstants;
 
-public class SettingsFragment extends Fragment implements CompoundButton.OnCheckedChangeListener, RadioGroup.OnCheckedChangeListener {
+public class SettingsFragment extends Fragment implements SharedPreferences.OnSharedPreferenceChangeListener, CompoundButton.OnCheckedChangeListener, RadioGroup.OnCheckedChangeListener {
 
     View rootView;
 
@@ -37,23 +38,7 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        preferences = getActivity().getSharedPreferences(TorchieConstants.PREF_KEY_APP, Context.MODE_PRIVATE);
-        prefEditor = preferences.edit();
-
-        sw_screen_on = (SwitchCompat) rootView.findViewById(R.id.sw_settings_screen_on);
-        sw_lock_screen = (SwitchCompat) rootView.findViewById(R.id.sw_settings_screen_lock);
-        sw_screen_off = (SwitchCompat) rootView.findViewById(R.id.sw_settings_screen_off);
-        rg_screen_off_options = (RadioGroup) rootView.findViewById(R.id.rg_screen_off_options);
-        et_screen_off_mins = (EditText) rootView.findViewById(R.id.et_settings_screen_off_minutes);
-        et_screen_off_sec = (EditText) rootView.findViewById(R.id.et_settings_screen_off_seconds);
-        cb_vibrate = (AppCompatCheckBox) rootView.findViewById(R.id.cb_vibrate);
-
-        sw_screen_on.setOnCheckedChangeListener(this);
-        sw_lock_screen.setOnCheckedChangeListener(this);
-        sw_screen_off.setOnCheckedChangeListener(this);
-        rg_screen_off_options.setOnCheckedChangeListener(this);
-        cb_vibrate.setOnCheckedChangeListener(this);
-
+        initUI();
         loadPreferences();
 
         return rootView;
@@ -69,20 +54,14 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
 
     @Override
     public void onPause() {
-        long min, sec;
-        try {
-            min = Long.valueOf(et_screen_off_mins.getText().toString());
-        } catch (Exception e) {
-            min = 0;
-        }
-        try {
-            sec = Long.valueOf(et_screen_off_sec.getText().toString());
-        } catch (Exception e) {
-            sec = TorchieConstants.DEFAULT_SCREENOFF_TIME;
-        }
-        long timeOut = ((min * 60) + sec) * 1000;
-        prefEditor.putLong(TorchieConstants.PREF_FUNC_SCREEN_OFF_TIME, timeOut).commit();
+        saveScreenOffPref();
         super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        preferences.unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
     }
 
     @Override
@@ -93,14 +72,8 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
             prefEditor.putBoolean(TorchieConstants.PREF_FUNC_SCREEN_LOCKED, isChecked).commit();
         } else if (buttonView.equals(sw_screen_off)) {
             prefEditor.putBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF, isChecked).commit();
-            setScreenOffOptionsUI(isChecked);
         } else if (buttonView.equals(cb_vibrate)) {
             prefEditor.putBoolean(TorchieConstants.PREF_FUNC_VIBRATE, isChecked).commit();
-            if (isChecked) {
-                Vibrator vib = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                if (vib.hasVibrator())
-                    vib.vibrate(TorchieConstants.DEFAULT_VIBRATOR_TIME);
-            }
         }
     }
 
@@ -112,11 +85,30 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
             } else if (checkedId == R.id.rb_settings_screen_off_timeout) {
                 prefEditor.putBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF_INDEFINITE, false).commit();
             }
-            setScreenOffOptionsUI(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF, false));
         }
     }
 
+    private void initUI() {
+        sw_screen_on = (SwitchCompat) rootView.findViewById(R.id.sw_settings_screen_on);
+        sw_lock_screen = (SwitchCompat) rootView.findViewById(R.id.sw_settings_screen_lock);
+        sw_screen_off = (SwitchCompat) rootView.findViewById(R.id.sw_settings_screen_off);
+        rg_screen_off_options = (RadioGroup) rootView.findViewById(R.id.rg_screen_off_options);
+        et_screen_off_mins = (EditText) rootView.findViewById(R.id.et_settings_screen_off_minutes);
+        et_screen_off_sec = (EditText) rootView.findViewById(R.id.et_settings_screen_off_seconds);
+        cb_vibrate = (AppCompatCheckBox) rootView.findViewById(R.id.cb_vibrate);
+
+        sw_screen_on.setOnCheckedChangeListener(this);
+        sw_lock_screen.setOnCheckedChangeListener(this);
+        sw_screen_off.setOnCheckedChangeListener(this);
+        rg_screen_off_options.setOnCheckedChangeListener(this);
+        cb_vibrate.setOnCheckedChangeListener(this);
+    }
+
     private void loadPreferences() {
+        preferences = getActivity().getSharedPreferences(TorchieConstants.PREF_KEY_APP, Context.MODE_PRIVATE);
+        prefEditor = preferences.edit();
+        preferences.registerOnSharedPreferenceChangeListener(this);
+
         sw_screen_on.setChecked(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_UNLOCKED, true));
         sw_lock_screen.setChecked(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_LOCKED, true));
         sw_screen_off.setChecked(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF, false));
@@ -124,35 +116,65 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
         cb_vibrate.setChecked(preferences.getBoolean(TorchieConstants.PREF_FUNC_VIBRATE, false));
     }
 
-    private void setScreenOffOptionsRadioUI(boolean indefiniteEnabled) {
-        if (indefiniteEnabled) {
-            rg_screen_off_options.check(R.id.rb_settings_screen_off_indefinite);
-        } else {
-            rg_screen_off_options.check(R.id.rb_settings_screen_off_timeout);
+    private void saveScreenOffPref() {
+        long min, sec;
+        String st_min, st_sec;
+
+        st_min = et_screen_off_mins.getText().toString();
+        st_sec = et_screen_off_sec.getText().toString();
+
+
+        try{
+            min = TextUtils.isEmpty(st_min) ? 0 : Long.parseLong(st_min);
+            sec = TextUtils.isEmpty(st_sec) ? 0 : Long.parseLong(st_sec);
+        }catch (Exception e){
+            min = 0;
+            sec = TorchieConstants.DEFAULT_SCREENOFF_TIME;
         }
+
+        long timeOut = ((min * 60) + sec) * 1000;
+        prefEditor.putLong(TorchieConstants.PREF_FUNC_SCREEN_OFF_TIME, timeOut).commit();
     }
 
     private void setScreenOffOptionsUI(boolean screenOffEnabled) {
         et_screen_off_sec.setText(String.valueOf((preferences.getLong(TorchieConstants.PREF_FUNC_SCREEN_OFF_TIME, TorchieConstants.DEFAULT_SCREENOFF_TIME) % 60000) / 1000), TextView.BufferType.EDITABLE);
         et_screen_off_mins.setText(String.valueOf(preferences.getLong(TorchieConstants.PREF_FUNC_SCREEN_OFF_TIME, TorchieConstants.DEFAULT_SCREENOFF_TIME) / 60000), TextView.BufferType.EDITABLE);
-        setScreenOffOptionsRadioUI(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF_INDEFINITE, false));
-        if (screenOffEnabled) {
-            for (int i = 0; i < rg_screen_off_options.getChildCount(); i++) {
-                rg_screen_off_options.getChildAt(i).setEnabled(true);
-            }
-            if (preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF_INDEFINITE, false)) {
-                et_screen_off_mins.setEnabled(false);
-                et_screen_off_sec.setEnabled(false);
-            } else {
-                et_screen_off_mins.setEnabled(true);
-                et_screen_off_sec.setEnabled(true);
-            }
+        for (int i = 0; i < rg_screen_off_options.getChildCount(); i++) {
+            rg_screen_off_options.getChildAt(i).setEnabled(screenOffEnabled);
+        }
+        if (preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF_INDEFINITE, false)) {
+            rg_screen_off_options.check(R.id.rb_settings_screen_off_indefinite);
         } else {
-            for (int i = 0; i < rg_screen_off_options.getChildCount(); i++) {
-                rg_screen_off_options.getChildAt(i).setEnabled(false);
-            }
-            et_screen_off_mins.setEnabled(false);
-            et_screen_off_sec.setEnabled(false);
+            rg_screen_off_options.check(R.id.rb_settings_screen_off_timeout);
+        }
+        boolean etEnableFlag = preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF, false) && !preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF_INDEFINITE, false);
+        et_screen_off_mins.setEnabled(etEnableFlag);
+        et_screen_off_sec.setEnabled(etEnableFlag);
+    }
+
+    private void vibrate(long time) {
+        Vibrator vib = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        if (vib.hasVibrator())
+            vib.vibrate(time);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case TorchieConstants.PREF_FUNC_SCREEN_OFF:
+                setScreenOffOptionsUI(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF, false));
+                break;
+            case TorchieConstants.PREF_FUNC_SCREEN_OFF_INDEFINITE:
+                setScreenOffOptionsUI(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF, false));
+                break;
+            case TorchieConstants.PREF_FUNC_SCREEN_OFF_TIME:
+                setScreenOffOptionsUI(preferences.getBoolean(TorchieConstants.PREF_FUNC_SCREEN_OFF, false));
+                break;
+            case TorchieConstants.PREF_FUNC_VIBRATE:
+                if (preferences.getBoolean(TorchieConstants.PREF_FUNC_VIBRATE, false)) {
+                    vibrate(TorchieConstants.DEFAULT_VIBRATOR_TIME);
+                }
+                break;
         }
     }
 }
