f83933fe2f41dfd4b96ec7a1a55e613be089e6ac
Merbin J Anselm <merbinjanselm@gmail.com>
2019-06-04 19:58:49 +0530

    Fixed issue where screenoff toggle rapidly fires

--- app/src/main/java/in/blogspot/anselmbros/torchie/main/manager/device/input/key/volume/rocker/VolumeKeyRocker.java
+++ app/src/main/java/in/blogspot/anselmbros/torchie/main/manager/device/input/key/volume/rocker/VolumeKeyRocker.java
@@ -17,6 +17,24 @@
  */
 
 /*
+ *     Copyright (C) 2017 Merbin J Anselm <merbinjanselm@gmail.com>
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc.,
+ *     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
  *     Copyright (C) 2016  Merbin J Anselm <merbinjanselm@gmail.com>
  *
  *     This program is free software; you can redistribute it and/or modify
@@ -80,7 +98,7 @@ public class VolumeKeyRocker extends VolumeKeyDevice {
         for (int i = 0; i < current_ptr; i++) {
             if (i > 0) {
                 int a = buffer[i], b = buffer[i - 1];
-                if (a == (-1 * b)) {
+                if ((a == -1 && b == 1) || (a == 1 && b == -1)) {
                     this.clearBuffer();
                     keyComboPerformed = true;
                 }
